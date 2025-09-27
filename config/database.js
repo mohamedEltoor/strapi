01acd1bf33fb98d3177eadb62b7143211beb0426
Dev : mohamed eltoor <dev.mohamedeltoor@gmail.com>
2025-09-27 19:55:08 +0300

    update content types

--- config/database.js
+++ config/database.js
@@ -1,10 +1,21 @@
+// module.exports = ({ env }) => ({
+//     connection: {
+//         client: 'postgres',
+//         connection: {
+//             connectionString: env('DATABASE_URL')
+//         },
+//         debug: true,
+//         pool: { min: 0, max: 7 },
+//     }
+// });
+
+
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_URL')
-        },
-        debug: true,
-        pool: { min: 0, max: 7 },
-    }
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+    },
+    useNullAsDefault: true,
+  },
 });
